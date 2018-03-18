58d607312d15115a67bc3cf51efeb29101ea9d59
Trevor Seitz <trevinla@gmail.com>
2018-03-18 17:37:16 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -11,13 +11,17 @@ class String
   end
 
   def exclamation?
-    self.end_with?{"!"}
+    self.end_with?("!")
   end
+
+  sentence_end = [".", "!", "?", "..."]
 
   def count_sentences
     count = 0
-    if sentence?
+    sentence_end.any {|punctuation| self.end_with?(punctuation)}
       count +=1
+
+
     end
     count
   end
